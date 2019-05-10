937b4ddd1ccfcf3b943de59aaff14365ad24d403
Samimsam94 <samimsam9489@gmail.com>
2019-05-10 11:56:46 +0600

    Light

---
+++ app/src/main/java/com/example/trafficcontrol/MainActivity.java
@@ -0,0 +1,40 @@
+package com.example.trafficcontrol;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+    public void trafficSignal(View view) {
+        Intent intent = new Intent(MainActivity.this, trafficLight.class);
+        startActivity(intent);
+
+    }
+
+    public void policeSignal(View view) {
+        Intent intent = new Intent(MainActivity.this, policeSignal.class);
+        startActivity(intent);
+    }
+
+    public void caution(View view) {
+        Intent intent = new Intent(MainActivity.this, caution.class);
+        startActivity(intent);
+    }
+
+    public void limit(View view) {
+        Intent intent = new Intent(MainActivity.this, limit.class);
+        startActivity(intent);
+    }
+
+    public void othersSignal(View view) {
+        Intent intent = new Intent(MainActivity.this, othersSignal.class);
+        startActivity(intent);
+    }
+}
